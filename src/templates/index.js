62dd3ac8ecc95d939540d737450b341b0256626b
Cristian Velasco Caceres <cristian@gradiweb.com>
2022-03-18 10:28:44 -0500

    staging clean up

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,10 +1,7 @@
+import "swiper/css";
 import "../scss/main.scss";
 import "../utils/hello";
-import {openAccordion} from "../utils/accordion";
 import "../components/slider-swiper";
-import { filter } from "../components/filter";
 import { variantOnChange } from "../components/variants-product";
 
-filter();
-variantOnChange('.variants');
-openAccordion();
+variantOnChange(".variants");
