d7f4d64552c739e93eb385b4d7f696773e8c74d0
entornoInteractivo <cristianvelasco1529@gmail.com>
2022-06-21 17:55:17 -0500

    update, conflicts and optimize accordions

--- src/utils/accordion.js
+++ src/utils/accordion.js
@@ -1,18 +1,19 @@
 import { $Qll } from "./query-selector";
 import { dataToggle } from "./toggle-dataset";
 
+/**
+ * Open accordion
+ * This function valuate all nodes with the class ".accordions" and add new event listener
+ */
 export function openAccordion() {
   $Qll('.accordions').forEach(
     accordion => {
-      $Qll('.accordion-item', accordion)
+      $Qll('.accordion-item__button', accordion)
         .forEach(item => {
           item.addEventListener(
             'click',
             function () {
-              if (accordion.dataset.toggle === 'true') {
-                allCloseAccordions('.accordion-item', accordion);
-              }
-              dataToggle(this);
+              accordionAction(this, accordion)
             }
           )
         })
@@ -20,10 +21,33 @@ export function openAccordion() {
   )
 }
 
-function allCloseAccordions (nodeAll, parent) {
-  return $Qll(nodeAll, parent).forEach(
-    node => {
-      node.dataset.active = 'false'
-    }
+/**
+ * Accordion Action
+ * This function change the dataset active in button element accordion (on click event)
+ * 
+ * @param {HTMLElement} element - Button to change data active
+ * @param {NodeListOf} accordions - All accordions in DOM
+ */
+function accordionAction(element, accordions) {
+  if (accordions.dataset.toggle === 'true') {
+    closeAll('.accordion-item', accordions);
+  }
+
+  dataToggle(element);
+}
+
+/**
+ * Loop to change all dataset active in button accordions
+ * 
+ * @param {String} nodeAll - Reference to search in parent element
+ * @param {HTMLElement} parent - Parent to valuate
+ * @returns Action to change dataset active to 'false' at all buttons
+ */
+function closeAll (nodeAll, parent) {
+  return $Qll(nodeAll, parent)
+    .forEach(node => (
+      node.children[0].dataset.active = 'false'
+    )
   )
 }
+ 
