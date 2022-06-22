eda0d906efb579a935ca4da16f659e7468002fc1
Miguel Soler <miguel@gradiweb.com>
2022-06-21 21:24:55 -0500

    add es6 rules

--- linter-rules/index.js
+++ linter-rules/index.js
@@ -2,12 +2,7 @@ module.exports = {
   extends: [
     './technical-bible',
     './errors',
-    './node',
-    './style',
-    './variables',
     './es6',
-    './imports',
-    './strict',
   ].map(require.resolve),
   parserOptions: {
     ecmaVersion: 2018,
--- src/utils/dom-elements.js
+++ src/utils/dom-elements.js
@@ -9,3 +9,5 @@ export const domElements = () => {
 
   return elements;
 }
+
+(a) =>{};
