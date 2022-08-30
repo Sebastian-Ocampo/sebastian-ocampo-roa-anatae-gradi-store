4f9ba55c8cb4be59768dfcd3064cc1ddcf3b3257
Rafael Andres Granados <rafael.granados@gradiweb.com>
2022-08-30 11:58:19 -0500

    merge branch

--- src/templates/index.js
+++ src/templates/index.js
@@ -2,12 +2,15 @@ import "swiper/css";
 import "../scss/main.scss";
 import "../utils/hello";
 import "../components/slider-swiper";
+import {openFormEdit} from "../components/form.addresses";
 import { variantOnChange } from "../components/variants-product";
 import { openAccordion } from "../utils/accordion";
 import { openDropDown } from "../components/header";
 import { toggleDataActive } from "../utils/toggle-dataset";
 
+
 variantOnChange(".variants");
 openAccordion();
 openDropDown();
 toggleDataActive("#burger-nav-js", "#nav-list-js");
+openFormEdit();
