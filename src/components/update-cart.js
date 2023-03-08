822d18ad88b903826f8a2ddbc7ac3a08e0435520
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2023-03-08 13:52:28 -0500

    add styles in side cart

--- src/components/update-cart.js
+++ src/components/update-cart.js
@@ -1,7 +1,7 @@
 import { stringToHTML } from '../utils/to-html';
 import { $Q, $Qll } from '../utils/query-selector';
-import { setQuantity } from "../utils/input-quantity";
-import { btnAddToCart, deleteItem, onChangeItemCart } from "./cart";
+import { setQuantity } from '../utils/input-quantity';
+import { btnAddToCart, deleteItem, onChangeItemCart } from './cart';
 import { barProgress } from '../utils/bar-progress';
 import { sliderUpsell } from './slider-swiper';
 
@@ -10,38 +10,33 @@ import { sliderUpsell } from './slider-swiper';
  * @param {string} str - String HTML of section rendeirng
  */
 export const updateCartItems = (str) => {
-  $Qll('.cartitems-js')
-    .forEach(
-      (element) => {
-        const elementRef = element;
-        elementRef.innerHTML = $Q(
-          '#cart-items',
-          stringToHTML(str),
-        ).outerHTML;
-      },
-    )
+  $Qll('.cartitems-js').forEach((element) => {
+    const elementRef = element;
+    elementRef.innerHTML = $Q('#cart-items', stringToHTML(str)).outerHTML;
+  });
 
   setQuantity();
   deleteItem();
   onChangeItemCart();
-}
+};
 
 /**
  * Update checkout button section in sidecart
  * @param {string} str - String HTML of section rendeirng
  */
- export const updateCartbutton = (str) => {
+export const updateCartbutton = (str) => {
   const inputBarProgress = $Q('#progress-bar-data', stringToHTML(str));
   const btnContainer = $Q('.cart-footer', stringToHTML(str));
   const domBtnContainer = $Qll('#container-footer-js');
 
+  console.log(inputBarProgress);
   barProgress(inputBarProgress);
 
   if (btnContainer) {
     domBtnContainer.forEach((element) => {
       const elementRef = element;
       elementRef.innerHTML = btnContainer.outerHTML;
-    })
+    });
 
     return;
   }
@@ -49,8 +44,8 @@ export const updateCartItems = (str) => {
   domBtnContainer.forEach((element) => {
     const elementRef = element;
     elementRef.innerHTML = '';
-  })
-}
+  });
+};
 
 /**
  * Chance all input value only cart page
@@ -58,14 +53,12 @@ export const updateCartItems = (str) => {
  * @param {String} id - Variant id item cart
  * @param {String} quantity - Quantity variant by item cart
  */
- const updateQuantity = (id, quantity) => {
-  $Qll(`.item-cart-js[id="${id}"]`).forEach(
-    (element) => {
-      const elementRef = element;
-      elementRef.value = quantity
-    },
-  )
-}
+const updateQuantity = (id, quantity) => {
+  $Qll(`.item-cart-js[id="${id}"]`).forEach((element) => {
+    const elementRef = element;
+    elementRef.value = quantity;
+  });
+};
 
 /**
  * Update price item for each item in cart items section
@@ -73,54 +66,47 @@ export const updateCartItems = (str) => {
  * @param {number} id - Product ID
  */
 export const updatePriceItem = (str, id) => {
+  const { dataset, outerText } = $Q(`#price-${id}`, stringToHTML(str));
 
-  const {
-    dataset,
-    outerText,
-  } = $Q(`#price-${id}`, stringToHTML(str));
-
-  $Qll(`.price-${id}`).forEach(
-    (element) => {
-      const elementRef = element;
-      elementRef.innerHTML = outerText
-    },
-  )
+  $Qll(`.price-${id}`).forEach((element) => {
+    const elementRef = element;
+    elementRef.innerHTML = outerText;
+  });
 
   updateQuantity(id, dataset.quantity);
-}
+};
 
 /**
  * Update total price in cart page
  * @param {string} str - String HTML of section rendeirng
  */
- export const updatetotalPrice = (str) => {
-
+export const updatetotalPrice = (str) => {
   if (!$Q('.cartpage-footer')) return;
-  if (!$Q("#total-price", stringToHTML(str))) {
+  if (!$Q('#total-price', stringToHTML(str))) {
     $Q('.cartpage-footer').style.display = 'none';
     return;
   }
 
-  if ($Q(".cartpage-footer__info--price") != null) {
-    $Q(".cartpage-footer__info--price").innerHTML = $Q(
-      "#total-price",
-      stringToHTML(str),
+  if ($Q('.cartpage-footer__info--price') != null) {
+    $Q('.cartpage-footer__info--price').innerHTML = $Q(
+      '#total-price',
+      stringToHTML(str)
     ).outerText;
   }
-}
+};
 
 /**
  * Update upsell section in sidecart
  * @param {string} str - String HTML of section rendeirng
  */
- export const updateUpsell = (str) => {
+export const updateUpsell = (str) => {
   if (!$Q('#upsell-js')) return;
 
   $Q('#upsell-js').innerHTML = $Q(
     '#cart-upsell-slider',
-    stringToHTML(str),
+    stringToHTML(str)
   ).outerHTML;
 
-  btnAddToCart(".add-product-cart-upsell");
+  btnAddToCart('.add-product-cart-upsell');
   sliderUpsell();
-}
+};
